74ce722296f09847636af98ff17a0e85c1ad8543
Taylor Rogers <taylor.rogers71@gmail.com>
2024-01-03 09:02:16 -0800

    30: done

--- 30.test.ts
+++ 30.test.ts
@@ -1,23 +1,45 @@
-import { findSubstring } from './30';
+import { findSubstring, checkForPermutationSubstr } from './30';
 
 const testFunction = findSubstring;
 
 type TestCase = { input: Parameters<typeof testFunction>; output: ReturnType<typeof testFunction> };
 const testCases: TestCase[] = [
+  { input: ['foob', ['foo', 'bar']], output: [] },
+  { input: ['foobar', ['foo', 'bar']], output: [0] },
+  { input: ['nonononononobarfoo', ['foo', 'bar']], output: [12] },
   { input: ['barfoothefoobarman', ['foo', 'bar']], output: [0, 9] },
   { input: ['wordgoodgoodgoodbestword', ['word', 'good', 'best', 'word']], output: [] },
   { input: ['barfoofoobarthefoobarman', ['bar', 'foo', 'the']], output: [6, 9, 12] },
   { input: ['aaaaaa', ['a', 'a', 'a']], output: [0, 1, 2, 3] },
   { input: ['anopeaaaaaa', ['a', 'a', 'a']], output: [5, 6, 7, 8] },
-  { input: ['barfoothefoobarman', ['foo', 'bar']], output: [0, 9] },
-  { input: ['barfoothefoobarman', ['foo', 'bar']], output: [0, 9] },
-  { input: ['barfoothefoobarman', ['foo', 'bar']], output: [0, 9] },
+  { input: ['aaaaaaaaaaaaaa', ['aa', 'aa']], output: [0,1,2,3,4,5,6,7,8,9,10] },
+  {
+    input: [
+      'abbaccaaabcabbbccbabbccabbacabcacbbaabbbbbaaabaccaacbccabcbababbbabccabacbbcabbaacaccccbaabcabaabaaaabcaabcacabaa',
+      ['cac', 'aaa', 'aba', 'aab', 'abc'],
+    ],
+    output: [97],
+  },
+  // { input: ['barfoothefoobarman', ['foo', 'bar']], output: [0, 9] },
 ];
 
 describe('findSubstring', () => {
   for (let testCase of testCases) {
-    it(`${JSON.stringify(...testCase.input)} => ${testCase.output}`, () => {
+    it(`${JSON.stringify(testCase.input)} => ${testCase.output}`, () => {
       expect(testFunction(...testCase.input)).toStrictEqual(testCase.output);
     });
   }
+});
+
+describe('checkForPermutationSubstr', () => {
+  it('"abc", ["c", "b", "a"] => true', () => {
+    expect(checkForPermutationSubstr('abc', ['c', 'b', 'a'])).toStrictEqual(true);
+  });
+
+  it('"aabcdcabc", ["abc", "cdc", "aab"] => true', () => {
+    expect(checkForPermutationSubstr('aabcdcabc', ['abc', 'cdc', 'aab'])).toStrictEqual(true);
+  });
+  it('"abc", ["a", "b", "z"] => true', () => {
+    expect(checkForPermutationSubstr('abc', ['a', 'b', 'z'])).toStrictEqual(false);
+  });
 });
--- 30.ts
+++ 30.ts
@@ -9,4 +9,57 @@
  *
  * Return the starting indices of all the concatenated substrings in s. You can return the answer in any order.
  */
-export function findSubstring(s: string, words: string[]): number[] {}
+export function findSubstring(s: string, words: string[]): number[] {
+  const fullSubstrLength = words.reduce((acc, curr) => acc + curr.length, 0);
+  const foundSubstrIndices = [];
+  // console.log({ s, words, fullSubstrLength });
+  if (s.length < fullSubstrLength) {
+    return [];
+  }
+  // console.log(
+    // `iterating from 0 -> ${s.length - fullSubstrLength}/${s.length} (-${fullSubstrLength}; ${words.join('|')})`
+  // );
+  for (let i = 0; i <= s.length - fullSubstrLength; i++) {
+    // console.log({ i });
+    const substr = s.substring(i, i + words[0].length);
+    const foundWordIndex = words.indexOf(substr);
+    if (foundWordIndex !== -1) {
+      const possibleRemainingPermutationStr = s.slice(i + words[0].length, i + fullSubstrLength);
+      const remainingWordsInPermutation = [...words];
+      remainingWordsInPermutation.splice(foundWordIndex, 1);
+      // console.log({ substr, foundWordIndex, possibleRemainingPermutationStr, remainingWordsInPermutation });
+      if (checkForPermutationSubstr(possibleRemainingPermutationStr, remainingWordsInPermutation)) {
+        // console.log(`Pushing index: ${i}`);
+        foundSubstrIndices.push(i);
+        // console.log({ i });
+        // i += words[0].length - 1;
+        // console.log({ i });
+      }
+    }
+  }
+  return foundSubstrIndices;
+}
+
+export function checkForPermutationSubstr(s: string, words: string[]): boolean {
+  // console.log({ checkForPermutationSubstr: true, s, words });
+  for (let word of words) {
+    let matchedIndex = undefined;
+    // console.log({ word, matchedIndex });
+    for (let i = 0; i < s.length; i += word.length) {
+      const currStr = s.slice(i);
+      // console.log({ currStr });
+      if (currStr.startsWith(word)) {
+        matchedIndex = i;
+        break;
+      }
+    }
+    if (matchedIndex === undefined) {
+      return false;
+    }
+    // console.log({ word, s });
+    s = s.slice(0, matchedIndex) + s.slice(matchedIndex + word.length);
+    // console.log({ word, s });
+  }
+  // console.log({ checkForPermutationSubstr__result: s.length === 0, s, words });
+  return s.length === 0;
+}
