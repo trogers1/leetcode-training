3d8140f0cd7cbb20f25eff2442b799afd6937e48
Taylor Rogers <taylor.rogers71@gmail.com>
2024-08-05 13:51:24 -0700

    1268

--- 1268.test.ts
+++ 1268.test.ts
@@ -1,4 +1,4 @@
-import { suggestedProducts } from './1268';
+import { suggestedProducts, lexicographicalCompare } from './1268';
 
 const testFunction = suggestedProducts;
 
@@ -14,13 +14,51 @@ const testCases: TestCase[] = [
       ['mouse', 'mousepad'],
     ],
   },
-  { input: [['havanna'], 'havana'], output: [['havana'], ['havana'], ['havana'], ['havana'], ['havana'], ['havana']] },
+  { input: [['havana'], 'havana'], output: [['havana'], ['havana'], ['havana'], ['havana'], ['havana'], ['havana']] },
+  { input: [['havana', 'havanna'], 'havana'], output: [['havana', 'havanna'], ['havana', 'havanna'], ['havana', 'havanna'], ['havana', 'havanna'], ['havana', 'havanna'], ['havana']] },
 ];
 
-describe('canVisitAllRooms', () => {
-  for (let testCase of testCases) {
-    it(`${JSON.stringify(testCase.input)} => ${testCase.output}`, () => {
-      expect(testFunction(...testCase.input)).toStrictEqual(testCase.output);
+describe('1268: canVisitAllRooms', () => {
+  describe('canVisitAllRooms', () => {
+    for (let testCase of testCases) {
+      it(`${JSON.stringify(testCase.input)} => ${testCase.output}`, () => {
+        expect(testFunction(...testCase.input)).toStrictEqual(testCase.output);
+      });
+    }
+  });
+  describe('lexicographicalCompare', () => {
+    it('should compare single letter strings', () => {
+      let result = ['a', 'b'].sort(lexicographicalCompare);
+      let expected = ['a', 'b'];
+      expect(result).toStrictEqual(expected);
+      result = ['b', 'a'].sort(lexicographicalCompare);
+      expected = ['a', 'b'];
+      expect(result).toStrictEqual(expected);
+      result = ['a', 'a'].sort(lexicographicalCompare);
+      expected = ['a', 'a'];
+      expect(result).toStrictEqual(expected);
     });
-  }
+    it('should compare same-length strings', () => {
+      let result = ['aaa', 'aab'].sort(lexicographicalCompare);
+      let expected = ['aaa', 'aab'];
+      expect(result).toStrictEqual(expected);
+      result = ['aab', 'aaa'].sort(lexicographicalCompare);
+      expected = ['aaa', 'aab'];
+      expect(result).toStrictEqual(expected);
+      result = ['aaaaaaa', 'aaabaaa'].sort(lexicographicalCompare);
+      expected = ['aaaaaaa', 'aaabaaa'];
+      expect(result).toStrictEqual(expected);
+    });
+    it('should compare diff-length strings', () => {
+      let result = ['mouse', 'mousepad'].sort(lexicographicalCompare);
+      let expected = ['mouse', 'mousepad'];
+      expect(result).toStrictEqual(expected);
+      result = ['mousepad', 'mouse'].sort(lexicographicalCompare);
+      expected = ['mouse', 'mousepad'];
+      expect(result).toStrictEqual(expected);
+      result = ['wxyz', 'abc', 'hijkl'].sort(lexicographicalCompare);
+      expected = ['abc', 'hijkl', 'wxyz'];
+      expect(result).toStrictEqual(expected);
+    });
+  });
 });
--- 1268.ts
+++ 1268.ts
@@ -37,4 +37,57 @@ Constraints:
 
 */
 
-export function suggestedProducts(products: string[], searchWord: string): string[][] {}
+export function lexicographicalCompare(str1: string, str2: string): number {
+  const shortestStrLength = str1.length < str2.length ? str1.length : str2.length;
+  for (let i = 0; i < shortestStrLength; i++) {
+    if (str1[i] === str2[i]) {
+      continue;
+    }
+    return str1.charCodeAt(i) - str2.charCodeAt(i);
+  }
+  if (str1.length === str2.length) {
+    // Same word
+    return 0;
+  }
+  return str1.length < str2.length ? -1 : 1;
+}
+
+export function suggestedProducts(products: string[], searchWord: string): string[][] {
+  let currSearchWord = '';
+  const suggestionResults: string[][] = [];
+  let currOrderedResults: string[] = [];
+  // console.log({ searchWord, products });
+
+  for (let i = 0; i < searchWord.length; i++) {
+    currSearchWord += searchWord[i];
+    currOrderedResults = [];
+    // console.log({ currSearchWord });
+    // TODO: remove products that don't match so we don't have to iterate over them in the future
+    for (const product of products) {
+      if (product.startsWith(currSearchWord)) {
+        // console.log({ product });
+        // TODO: Maybe add in the correct lexicographical order to remove final sorting step?
+        currOrderedResults.push(product);
+      }
+    }
+    // TODO: Come back and sort this lexicographically (char codes?)
+    suggestionResults.push(currOrderedResults.sort(lexicographicalCompare).slice(0, 3));
+    // console.log({ suggestionResults });
+  }
+  return suggestionResults;
+}
+
+// Found this interesting, simple solution as well. Apparently the sort function is good enough, and sorting first is pretty sneaky.
+/* function suggestedProducts(products: string[], searchWord: string): string[][] {
+    products.sort();
+    const results: string[][] = [];
+    for (let i = 0; i < searchWord.length; i++) {
+        const suggestions: string[] = [];
+        for (const product of products) {
+            if (suggestions.length === 3) break;
+            if (product.startsWith(searchWord.slice(0, i + 1))) suggestions.push(product);
+        }
+        results.push(suggestions);
+    }
+    return results;
+} */
